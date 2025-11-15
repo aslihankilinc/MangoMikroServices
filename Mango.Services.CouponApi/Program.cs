d0ac1f364b4e552c9fdc3e82d0c8efba6bfb7750
aslı kılıç <aslhanklnc@gmail.com>
2025-11-15 16:26:16 +0300

    db migration

--- Mango.Services.CouponApi/Program.cs
+++ Mango.Services.CouponApi/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
-
+using SQLitePCL;
 var builder = WebApplication.CreateBuilder(args);
-
+Batteries.Init();
 // Add services to the container.
 builder.Services.AddDbContext<Mango.Services.CouponApi.Data.AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("MangoContext"));
 });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
