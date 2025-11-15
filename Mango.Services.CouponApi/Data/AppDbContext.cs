441e4eafa7b3e17449866d3c050830b7d6281787
aslı kılıç <aslhanklnc@gmail.com>
2025-11-15 16:18:45 +0300

    Add project files.

---
+++ Mango.Services.CouponApi/Data/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Mango.Services.CouponApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponApi.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
+
+/*
+protected readonly IConfiguration configuration;
+private string _connectionStringOrName;
+
+public AppDbContext(string connectionStringOrName)
+{
+    this._connectionStringOrName = connectionStringOrName;
+    var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+    configuration = builder;
+}
+public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions) { }
+protected override void OnConfiguring(DbContextOptionsBuilder options)
+{
+    // connect to sqlite database
+    string connectionString = string.Empty;
+    connectionString = configuration.GetConnectionString("DbContext");
+    var result = options.UseSqlite(connectionString);
+}
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    base.OnModelCreating(modelBuilder);
+}*/
