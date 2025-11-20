ffafdb3aeba6418b8a1c3b3d156da3d01d22a236
aslı kılıç <aslhanklnc@gmail.com>
2025-11-20 23:04:06 +0300

    +++addd auth view

---
+++ Mango.Web.UI/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Mango.Web.UI.IContract;
+using Mango.Web.UI.Models.Dto;
+using Mango.Web.UI.Models.Dto.Auth;
+using Mango.Web.UI.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace Mango.Web.UI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto responseDto = await _authService.LoginAsync(obj);
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto =
+                    JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+               
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["error"] = responseDto.Message;
+                return View(obj);
+            }
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=Const.Admin,Value=Const.Admin},
+                new SelectListItem{Text=Const.Customer,Value=Const.Customer},
+            };
+
+            ViewBag.RoleList = roleList;
+            return View();
+        }
+
+    }
+}
---
+++ Mango.Web.UI/Utility/Const.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.UI.Utility
+{
+    public class Const
+    {
+       //auth
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string TokenCookie = "JWTToken";
+    }
+}
