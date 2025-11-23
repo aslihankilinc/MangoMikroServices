08021975bcd962a01e5e0d5a2892981f453c4362
aslı kılıç <aslhanklnc@gmail.com>
2025-11-23 20:33:18 +0300

    ++product api migration

---
+++ Mango.Services.ProductApi/Migrations/20251123173136_AddProductToDb.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    CategoryName = table.Column<string>(type: "TEXT", nullable: false),
+                    ImageUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageLocalPath = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageLocalPath", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", "Ekmek", null, "https://placehold.co/603x403", "Ekmek", 15.0 },
+                    { 2, "Appetizer", "Kepekli Ekmek", null, "https://placehold.co/602x402", "Kepekli Ekmek", 13.99 },
+                    { 3, "Dessert", "Çavdar Ekmeği", null, "https://placehold.co/601x401", "Çavdar Ekmeği", 10.99 },
+                    { 4, "Entree", "Simit", null, "https://placehold.co/600x400", "Simit", 15.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
