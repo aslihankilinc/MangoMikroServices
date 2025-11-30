8e95942d326e8a30b53bd4bfcffefe9e761a91a6
aslı kılıç <aslhanklnc@gmail.com>
2025-11-30 20:12:56 +0300

    ShoppincartApi  migration

--- Mango.Services.ShoppingCartApi/Data/AppDbContext.cs
+++ Mango.Services.ShoppingCartApi/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿
+using Mango.Services.ShoppingCartApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartApi.Data
@@ -6,7 +7,8 @@ namespace Mango.Services.ShoppingCartApi.Data
     public class AppDbContext:DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);         
--- Mango.Services.ShoppingCartApi/MappingConfig.cs
+++ Mango.Services.ShoppingCartApi/MappingConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Mango.Services.ShoppingCartApi.Models;
+using Mango.Services.ShoppingCartApi.Models.Dto;
 namespace Mango.Services.ShoppingCartApi
 {
     public class MappingConfig
@@ -6,6 +8,10 @@ namespace Mango.Services.ShoppingCartApi
         public static MapperConfiguration RegisterMaps()
         {
             var expression = new MapperConfigurationExpression();
+            expression.CreateMap<CartHeaderDto, CartHeader>();
+            expression.CreateMap<CartHeader, CartHeaderDto>();
+            expression.CreateMap<CartDetailsDto, CartDetails>();
+            expression.CreateMap<CartDetails, CartDetailsDto>();
             var config = new MapperConfiguration(expression);
             return config;
         }
