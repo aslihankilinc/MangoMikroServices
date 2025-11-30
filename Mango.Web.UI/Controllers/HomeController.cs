3e9a64ad8b96624d5b513412274ce7d2fe40ee01
aslı kılıç <aslhanklnc@gmail.com>
2025-11-30 18:49:14 +0300

    +++

--- Mango.Web.UI/Controllers/HomeController.cs
+++ Mango.Web.UI/Controllers/HomeController.cs
@@ -1,23 +1,43 @@
-﻿using Mango.Web.UI.Models;
+﻿using Mango.Web.UI.IContract;
+using Mango.Web.UI.Models;
+using Mango.Web.UI.Models.Dto;
+using Mango.Web.UI.Models.Dto.Product;
 using Mango.Web.UI.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+       
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto>? list = new();
+
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(list);
         }
 
         //rol eklendi
